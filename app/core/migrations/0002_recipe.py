74923ee0f6f8b719c3b64dc081ce61c3f029f62e
Illia Yurchenko <iliayurchenko06@gmail.com>
2026-02-18 16:46:19 +0200

    Add description field to Recipe model in migration

--- app/core/migrations/0002_recipe.py
+++ app/core/migrations/0002_recipe.py
@@ -15,6 +15,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
+                ('description', models.TextField(blank=True)),
                 ('time_minutes', models.IntegerField()),
                 ('price', models.DecimalField(decimal_places=2, max_digits=5)),
                 ('link', models.CharField(blank=True, max_length=255)),
